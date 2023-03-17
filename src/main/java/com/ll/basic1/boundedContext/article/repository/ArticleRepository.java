6bcbc777bfd0d93392e022e57412ca1b5eea7cac
PARK-MI-JI <p36912@naver.com>
2023-03-17 16:03:46 +0900

    오류

--- src/main/java/com/ll/basic1/boundedContext/article/repository/ArticleRepository.java
+++ src/main/java/com/ll/basic1/boundedContext/article/repository/ArticleRepository.java
@@ -1,6 +1,6 @@
 package com.ll.basic1.boundedContext.article.repository;
 
-import com.ll.basic1.boundedContext.article.entitiy.Article;
+import com.ll.basic1.boundedContext.article.entity.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ArticleRepository extends JpaRepository<Article, Long> {
